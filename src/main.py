2f1da25ee5c8e75341efc23735c7367e6c8877b5
bafflingbug <i@bafflingbug.cn>
2018-05-07 12:33:33 +0800

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -19,4 +19,6 @@ app.logger.addHandler(fh)
 load_plugins(app)
 
 if __name__ == '__main__':
+    from werkzeug.contrib.fixers import ProxyFix
+    app.wsgi_app = ProxyFix(app.wsgi_app)
     app.run(debug=False, host='0.0.0.0', port=80)
