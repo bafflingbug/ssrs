021d5d9df49d8a5a834b68b9f1e5cdff1f10fd40
bafflingbug <i@bafflingbug.cn>
2019-04-03 11:12:02 +0800

    新方案

--- src/service/plugins/v2s/__init__.py
+++ src/service/plugins/v2s/__init__.py
@@ -84,7 +84,16 @@ def ssr_load():
     sers = []
     for service in services:
         con = safe_get(service, 'config')
-        port = safe_get(service, 'port')
+        ctips = safe_get(service, 'tips')
+        tips = {}
+        if ctips is not None:
+            for item in ctips:
+                oport = item['origin_port']
+                i = {}
+                for k, v in item.iteritems():
+                    if k != 'origin_port':
+                        i[k] = v
+                tips[int(oport)] = i
         if not con and con == '':
             raise ValueError('v2ray config not find')
         host = get_host()
@@ -92,7 +101,7 @@ def ssr_load():
             raise ValueError('\'host\' is config is None')
         remarks = safe_value(safe_get(service, 'remarks'), 'default')
         restart = safe_value(safe_get(service, 'restart'), '')
-        ssr = v2ray(con, host, port, remarks, restart)
+        ssr = v2ray(con, tips, host, remarks, restart)
         sers.extend(ssr.get_services())
     return sers
 
--- src/service/plugins/v2s/v2ray.py
+++ src/service/plugins/v2s/v2ray.py
@@ -39,62 +39,71 @@ class v2ray:
                 return None
             return base64.urlsafe_b64encode(json.dumps(self.conf).encode()).decode()
 
-    def __init__(self, conf, host, port, remarks, restart):
+    def __init__(self, conf, tips, host, remarks, restart):
         self.data_list = []
         self.conf = conf
         with open(self.conf, 'r') as f:
             try:
-                c = json.load(f)
-                self.config = safe_get(c, "inbound")
-                if self.config is None or safe_get(self.config, 'protocol') != 'vmess':
-                    raise ValueError('not find vmess config')
+                self.configs = json.load(f)
+                # self.config = safe_get(c, "inbound")
+                # if self.config is None or safe_get(self.config, 'protocol') != 'vmess':
+                #     raise ValueError('not find vmess config')
             except Exception:
                 raise ValueError('v2ray config is not json: %s' % conf)
         self.host = host
         self.remarks = remarks
         self.restart = restart
-        self.port = port
+        self.tips = tips
 
     def get_services(self):
-        _net = self.config['streamSettings']['network']
-        _tls = self.config['streamSettings']['security']
-        _path = ""
-        _type = 'none'
-        if _net == 'ws':
-            _path = self.config['streamSettings']['wsSettings']['path']
-        elif _net == 'h2':
-            _path = self.config['streamSettings']['httpSettings']['path']
-        elif _net == 'tcp':
-            _type = self.config['streamSettings']['tcpSettings']['header']['type']
-        elif _net == 'kcp':
-            _type = self.config['streamSettings']['kcpSettings']['header']['type']
-        elif _net == 'quic':
-            _type = self.config['streamSettings']['quicSettings']['header']['type']
-        base_service = v2ray.Service(
-            {
-                'v': '2',
-                'add': self.host,
-                'net': _net,
-                'id': '',
-                'aid': 0,
-                'type': _type,
-                'ps': self.remarks,
-                'host': self.host if _net in ('ws', "h2") else "",
-                'path': _path,
-                'tls': _tls,
-                'restart': self.restart,
-                'port': self.config['port'] if self.port is None else self.port
-            }
-        )
-        for item in self.config['settings']['clients']:
-            service = copy.deepcopy(base_service)
-            a = {
-                'id': item['id'],
-                'aid': item['alterId']
-            }
-            service.update(a)
-            url = service.get_data()
-            if url is None:
-                continue
-            self.data_list.append(url)
+        if safe_get(self.configs, 'inbounds') is not None:
+            c = safe_get(self.configs, 'inbounds')
+        elif safe_get(self.configs, 'inbound') is not None:
+            c = [safe_get(self.configs, 'inbound')]
+        else:
+            raise ValueError('not find vmess config')
+        for config in c:
+            _net = config['streamSettings']['network']
+            _path = ""
+            _type = 'none'
+            if _net == 'ws':
+                _path = config['streamSettings']['wsSettings']['path']
+            elif _net == 'h2':
+                _path = config['streamSettings']['httpSettings']['path']
+            elif _net == 'tcp':
+                _type = config['streamSettings']['tcpSettings']['header']['type']
+            elif _net == 'kcp':
+                _type = config['streamSettings']['kcpSettings']['header']['type']
+            elif _net == 'quic':
+                _type = config['streamSettings']['quicSettings']['header']['type']
+            base_service = v2ray.Service(
+                {
+                    'v': '2',
+                    'add': self.host,
+                    'net': _net,
+                    'id': '',
+                    'aid': 0,
+                    'type': _type,
+                    'ps': self.remarks,
+                    'host': self.host if _net in ('ws', "h2") else "",
+                    'path': _path,
+                    'tls': config['streamSettings']['security'],
+                    'restart': self.restart,
+                    'port': config['port']
+                }
+            )
+            _port = config['port']
+            if int(_port) in self.tips:
+                base_service.update(self.tips[int(_port)])
+            for item in config['settings']['clients']:
+                service = copy.deepcopy(base_service)
+                a = {
+                    'id': item['id'],
+                    'aid': item['alterId']
+                }
+                service.update(a)
+                url = service.get_data()
+                if url is None:
+                    continue
+                self.data_list.append(url)
         return self.data_list
